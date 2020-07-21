5572f65c753a5749cab2e98ad4cc201f0f8e3e32
biquando <biquando@gmail.com>
2020-07-21 16:39:34 -0700

    Updates

---
+++ precache-manifest.570c848a191fdfeab74742cd2276078f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2425882d9143d56c9019015f95e87e4",
+    "url": "/blap/index.html"
+  },
+  {
+    "revision": "eb2455bf7e52670b7e26",
+    "url": "/blap/static/css/main.7ecd8cb7.chunk.css"
+  },
+  {
+    "revision": "1cd4ba8c1baa79de1ea4",
+    "url": "/blap/static/js/2.8114eebf.chunk.js"
+  },
+  {
+    "revision": "08939e39b52bff53371a59c0cd97d6d4",
+    "url": "/blap/static/js/2.8114eebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2455bf7e52670b7e26",
+    "url": "/blap/static/js/main.4a2392bc.chunk.js"
+  },
+  {
+    "revision": "d24b80df9bbd325686c9",
+    "url": "/blap/static/js/runtime-main.7c2c1014.js"
+  }
+]);
